0780d93f6fb30200381bbb9f0fefc56ecfbe3796
Darragh Murphy <dmurphy@servuo.com>
2023-05-02 18:17:05 -0700

    Add eqArrays Function

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `Assertion Passed: ${actual} === ${expected}`;
+  }
+  return `Assertion Failed: ${actual} !== ${expected}`;
+
+};
+
+const eqArrays = function(arr, arr2) {
+  if (arr.length !== arr2.length)
+    return false;
+
+  for (let i = 0; i < arr.length; i++)
+    if (arr[i] !== arr2[i])
+      return false;
+
+  return true;
+
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
