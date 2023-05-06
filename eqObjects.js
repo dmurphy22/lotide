c1e097b3efb51c114cf8d12ab42a3b8d00ec08eb
Darragh Murphy <dmurphy@servuo.com>
2023-05-05 19:34:58 -0700

    Use recursion in eqObejects

--- eqObjects.js
+++ eqObjects.js
@@ -10,29 +10,33 @@ const eqArrays = function(arr, arr2) {
 
 };
 
-const eqObjects = function(object1, object2) {
+const eqObjects = function(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
 
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
+  if (keys1.length !== keys2.length) {
     return false;
   }
 
-  for (let key in object1) {
-    if (!Object.prototype.hasOwnProperty.call(object2, key)) {
+  for (const key of keys1) {
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+
+    if (Array.isArray(val1) && Array.isArray(val2)) {
+      if (!eqArrays(val1, val2)) {
+        return false;
+      }
+    } else if (typeof val1 === "object" && typeof val2 === "object") {
+      if (!eqObjects(val1, val2)) {
+        return false;
+      }
+    } else if (val1 !== val2) {
       return false;
     }
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key],object2[key])) {
-        return false;
-
-      }
-    } else if (object1[key] !== object2[key])
-      return false;
   }
 
   return true;
-
 };
-
 
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
@@ -40,3 +44,8 @@ eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject); // => true
 
 const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
 eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject);
+
+eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }); // => true
+
+eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }); // => false
+eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 });
