0c5353910e56b71467d8ddf25a979c8977883b58
Darragh Murphy <dmurphy@servuo.com>
2023-05-08 16:02:08 -0700

    implement testing on middle.

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(arr, arr2) {
-  if (arr.length !== arr2.length)
-    return false;
-
-  for (let i = 0; i < arr.length; i++)
-    if (arr[i] !== arr2[i])
-      return false;
-
-  return true;
-
-};
-
-const assertArraysEqual = function(arr, arr2) {
-  if (eqArrays(arr,arr2)) {
-    console.log(`Assertion Passed: ${arr} === ${arr2}`);
-    return;
-  }
-  console.log(`Assertion Failed: ${arr} !== ${arr2}`);
-
-};
-
 const middle = function(array) {
   if (array.length < 3)
     return [];
@@ -32,8 +11,5 @@ const middle = function(array) {
   return [array[midpoint]];
 
 };
-assertArraysEqual(middle([1, 2, 3]), [ 2 ]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [ 3, 4 ]);
-middle([1, 2, 3]);
-middle([1, 2, 3, 4, 5]);
-middle([1, 2, 3, 4, 5, 6]);
+
+module.exports = middle;
