89cf455af6e9045bb6369813ce4bcdbf5a00ccff
Darragh Murphy <dmurphy@servuo.com>
2023-05-08 14:22:12 -0700

    implement assertEqual into eqArraysTest

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,7 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
 
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), true); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), true); // => false
